b32083b22a012539d1d25411a13262437150b522
Toni Le <toni.le@outlook.de>
2021-01-14 21:32:34 +0100

    main skeleton

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func scan(path string) {
+	fmt.Println("Scanning folger...")
+}
+
+func stats(email string) {
+	print("Displaying stats...")
+}
+
 func main() {
-	fmt.Println("Hello World!")
+
 }
