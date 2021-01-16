3e4690c0f716e3fc96a7433ace3dbc3ea0cb3f06
Toni Le <toni.le@outlook.de>
2021-01-16 21:08:42 +0100

    Skeleton reflection workflow

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func scan(path string) {
 	fmt.Println("Scanning folger...")
@@ -11,5 +14,19 @@ func stats(email string) {
 }
 
 func main() {
+	var (
+		folder string
+		email  string
+	)
 
+	flag.StringVar(&folder, "add", "", "Add new folder to scan for git repositories")
+	flag.StringVar(&email, "email", "user@email.com", "Email to scan")
+	flag.Parse()
+
+	if folder != "" {
+		scan(folder)
+		return
+	}
+
+	stats(email)
 }
