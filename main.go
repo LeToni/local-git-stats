c9dbb0c15899d8284482f798cda80760918769b4
Toni Le <toni.le@outlook.de>
2021-01-16 21:26:19 +0100

    scanning folder for git repositories and adding it to existing list of found git repos

--- main.go
+++ main.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strings"
 )
 
 func scan(folder string) {
 	repositories := recursiveScanFolder(folder)
 	filepath := getDotFilePath()
 	addNewFoundRepositories(filePath, repositories)
+}
+
+func scanGitFolders(folders []string, folder string) []string {
+	folder = strings.TrimSuffix(folder, "/")
+	f, err := os.Open(folder)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	files, err := f.Readdir(-1)
+	if err != nil {
+		panic(err)
+	}
+
+	var path string
+
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				folders = append(folders, path)
+			}
+			folders = scanGitFolders(folders, path)
+		}
+	}
+
+	return folders
 }
 
 func stats(email string) {
