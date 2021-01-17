ccc6642132d1c5f698918b3ca5858967e126c6f6
Toni Le <toni.le@outlook.de>
2021-01-17 20:52:17 +0100

    (positiv) Unittest recursivescanfolder

--- main_test.go
+++ main_test.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestRecursiveScanFolder(t *testing.T) {
+	dir, _ := os.Getwd()
+
+	outPutDir := strings.Join(recursiveScanFolder(dir), "")
+
+	if outPutDir != dir {
+		t.Error("Has not found correct repo foolder", dir)
+	}
+}
