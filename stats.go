84d90b60c3da346be9dd9bb286a7abae82b71b59
Toni Le <toni.le@outlook.de>
2021-01-31 18:00:02 +0100

    Processing commits

---
+++ stats.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+const (
+	outOfRange          = 99999
+	daysInLastSixMonths = 183
+)
+
+func stats(email string) {
+	commits := processRepos(email)
+	printCommitStats(commits)
+}
+
+func processRepos(email string) *map[int]int {
+	filePath := getDotFilePath()
+	repos := parseDotFile(filePath)
+	daysInMap := daysInLastSixMonths
+
+	commits := make(map[int]int, daysInMap)
+
+	for i := daysInMap; i > 0; i-- {
+		commits[i] = 0
+	}
+
+	for _, path := range repos {
+		fillCommits(email, path, &commits)
+	}
+
+	return &commits
+}
+
+func fillCommits(email string, path string, commits *map[int]int) *map[int]int {
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		panic(err)
+	}
+
+	ref, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+
+	iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		panic(err)
+	}
+
+	offset := calcOffSet()
+	err = iterator.ForEach(func(c *object.Commit) error {
+		daysAgo := countDaysSinceDate(c.Author.When) + offset
+
+		if c.Author.Email != email {
+			return nil
+		}
+
+		if daysAgo != outOfRange {
+			(*commits)[daysAgo]++
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return commits
+}
+
+func countDaysSinceDate(date time.Time) int {
+	days := 0
+	now := getBeginningOfDay(time.Now())
+
+	for date.Before(now) {
+		date = date.Add(time.Hour * 24)
+		days++
+		if days > daysInLastSixMonths {
+			return outOfRange
+		}
+	}
+	return days
+}
+
+func getBeginningOfDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	startOfDay := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+
+	return startOfDay
+}
+
+func calcOffSet() (offset int) {
+
+	weekday := time.Now().Weekday()
+	switch weekday {
+	case time.Sunday:
+		offset = 7
+	case time.Monday:
+		offset = 6
+	case time.Tuesday:
+		offset = 5
+	case time.Wednesday:
+		offset = 4
+	case time.Thursday:
+		offset = 3
+	case time.Friday:
+		offset = 2
+	case time.Saturday:
+		offset = 1
+	}
+
+	return offset
+}
